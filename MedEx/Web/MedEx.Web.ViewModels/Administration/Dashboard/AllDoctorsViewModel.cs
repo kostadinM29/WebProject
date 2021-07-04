a2743015e038e8d4cb479d8fcecf90a5dd7122bb
Kostadin <minovkostadin@gmail.com>
2021-07-04 18:46:18 +0300

    fixed admin/alldoctors a little

---
+++ MedEx/Web/MedEx.Web.ViewModels/Administration/Dashboard/AllDoctorsViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace MedEx.Web.ViewModels.Administration.Dashboard
+{
+    public class AllDoctorsViewModel
+    {
+        public string FullName { get; set; }
+
+        public int Age { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public int? Experience { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string Biography { get; set; }
+
+        public string Town { get; set; }
+
+        public string Specialization { get; set; }
+
+        public bool HasApplied { get; set; }
+
+        public bool IsValidated { get; set; }
+    }
+}
--- MedEx/Web/MedEx.Web/Areas/Administration/Controllers/DashboardController.cs
+++ MedEx/Web/MedEx.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -1,16 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MedEx.Data.Common.Repositories;
+using MedEx.Data.Models;
+using MedEx.Web.ViewModels.Administration.Dashboard;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace MedEx.Web.Areas.Administration.Controllers
 {
     public class DashboardController : AdministrationController
     {
-        public DashboardController()
+        private readonly IDeletableEntityRepository<Doctor> _doctorRepository;
+        private readonly IDeletableEntityRepository<Specialization> _specializationRepository;
+        private readonly IRepository<Town> _townRepository;
+
+        public DashboardController(IDeletableEntityRepository<Doctor> doctorRepository, IDeletableEntityRepository<Specialization> specializationRepository, IRepository<Town> townRepository)
         {
+            _doctorRepository = doctorRepository;
+            _specializationRepository = specializationRepository;
+            _townRepository = townRepository;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult AllDoctors()
+        {
+            var model = _doctorRepository.All()
+                .Select(d => new AllDoctorsViewModel
+                {
+                    FullName = d.FirstName + d.LastName,
+                    Age = d.Age,
+                    PhoneNumber = d.PhoneNumber,
+                    Experience = d.Experience,
+                    Address = d.Address,
+                    Email = d.Email,
+                    Biography = d.Biography,
+                    Town = _townRepository.AllAsNoTracking().FirstOrDefault(t => t.Id == d.TownId).Name,
+                    Specialization = _specializationRepository.AllAsNoTracking()
+                        .FirstOrDefault(s => s.Id == d.SpecializationId).Name,
+                    HasApplied = d.HasApplied
+                })
+                .ToList();
+
+            return View(model);
         }
     }
 }
